f4a211933cd9eac9a4df712d42e7b87f6e5b3935
Cody Goodman <codygman.consulting@gmail.com>
2016-09-10 01:02:05 -0500

    Intial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# trivial-flycheck-linter
---
+++ flycheck-definition.el
@@ -0,0 +1,10 @@
+;; 1. Put hello-linter somewhere on your path
+;; 2. open trivial.json
+;; 3. M-x flycheck-compiler
+;; 4. select "hello-linter"
+;; 5. see list of errors
+(flycheck-define-checker hello-linter
+  "The most trivial hello world linter I could think up"
+  :command ("hello-linter" source-original)
+  :error-patterns ((error line-start (and not-newline) line-end))
+  :modes (json-mode))
